766d12bfd8810cfa2e29765f080d8b928674625a
jee255401 <jee2554@naver.com>
2018-10-06 03:06:29 +0900

    Changed ...

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-
+from .models import Bookmark
 # Create your views here.
 
 from django.views.generic import ListView, DetailView
 from bookmark.models import Bookmark
+
+def tabularBookmark(request):
+    bookmarks = Bookmark.objects.all().order_by('id')
+    return render(request, 'bookmark/tabular_list.html', {'bookmarks': bookmarks})
 
 class BookmarkLV(ListView):
     model = Bookmark
 
 class BookmarkDV(DetailView):
     model = Bookmark
-
-def tabularBookmark(request):
-    bookmarks = Bookmark.objects.all().order_by('id')
-    return render(request, 'bookmark/tabular_list.html', {'bookmarks': bookmarks})
