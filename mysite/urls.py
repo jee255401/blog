a56088c8fa175693c9184937ad765251c7745722
jee255401 <jee2554@naver.com>
2018-10-28 22:51:42 +0900

    Changed ...

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-# from bookmark.views import BookmarkLV, BookmarkDV
+from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
